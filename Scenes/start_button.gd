e39437c89a003bc524b22382ae7b0fb95ae39ea2
Noam Ben Simon <noampvb@gmail.com>
2025-12-14 00:05:25 -0500

    Pushing updates to main

--- Scenes/start_button.gd
+++ Scenes/start_button.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scence_to_file()
+	get_tree().change_scene_to_file("Level1.tscn")
 
 
 func _on_exit_pressed() -> void:
