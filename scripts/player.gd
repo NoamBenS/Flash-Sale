0d9ef051e3926e70b66520f5d7891a92072cccfe
Noam Ben Simon <noampvb@gmail.com>
2025-12-13 23:58:16 -0500

    Reapply "Merge branch 'master' into background"
    This reverts commit ca618a5ba0e565a5d446c79645702acf710fc592.

---
+++ scripts/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+const speed = 100
+
+@onready var anim := $"Walk Animations"
+
+var facing := Vector2.DOWN
+
+func _physics_process(delta):
+	
+	var direction := Vector2.ZERO
+
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+		facing = direction
+
+	velocity = direction * speed
+	update_animation()
+	move_and_slide()
+	
+func update_animation():
+	if velocity == Vector2.ZERO:
+		if (facing.y > 0):
+			anim.play("idle_down")
+		elif (facing.y < 0):
+			anim.play("idle_up")
+		else:
+			if (facing.x > 0):
+				anim.play("idle_right")
+			elif (facing.x < 0):
+				anim.play("idle_left")
+		return
+
+	if abs(facing.x) > abs(facing.y):
+		if facing.x > 0:
+			anim.play("walk_right")
+		else:
+			anim.play("walk_left")
+	else:
+		if facing.y > 0:
+			anim.play("walk_down")
+		else:
+			anim.play("walk_up")
+	return
