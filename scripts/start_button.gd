f211b959280ed96e79e1aa8f1c00ccabf0736334
Noam Ben Simon <noampvb@gmail.com>
2025-12-14 00:09:01 -0500

    renamed files, start game works

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("Level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
 
 
 func _on_exit_pressed() -> void:
