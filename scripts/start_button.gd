79441a467ac94a97855dc337ecd1461e72af64e4
Noam Ben Simon <noampvb@gmail.com>
2025-12-17 18:44:05 -0500

    updating referenced names

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -12,7 +12,7 @@ func _process(_delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
 
 func _on_exit_pressed() -> void:
--- scripts/victory_screen.gd
+++ scripts/victory_screen.gd
@@ -4,4 +4,4 @@ func _on_mainmenubutton_pressed():
 	get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
 
 func _on_replaybutton_pressed():
-	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
